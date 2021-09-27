d7ebad2206e717dfbac30018dee4bd05f7dfc09d
Jorge Cruz <joshiecruz@gmail.com>
2021-09-26 21:55:25 -0500

    Eliminar Archivo Jhint

--- src/pages/roleManager.jsx
+++ src/pages/roleManager.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
 
 export const roleManager = () => {
     return (
         <div>
-            
+         AAAAAAaaa   
         </div>
     )
 }
+export default roleManager;
