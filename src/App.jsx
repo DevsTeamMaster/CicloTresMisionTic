f6fbac063cbc714813ef828a637dff93eab8336b
GPFrederick <gpf95@protonmail.com>
2021-09-28 18:23:08 -0500

    Agregar página de agregar ventas al layout principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 //import Layout from 'layouts/Layout';
-<<<<<<< HEAD
 import Index from "pages";
 import Bordercollie from "pages/bordercollie";
 import RhodesianInfoPage from "pages/rhodesian";
@@ -10,23 +9,13 @@ import Navbar from "components/Navbar";
 import AddSales from "pages/addSales";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "styles/styles.scss";
-=======
-import Index from 'pages';
-import Bordercollie from 'pages/bordercollie';
-import RhodesianInfoPage from 'pages/rhodesian';
-import Login from 'pages/login';
-import Register from 'pages/register';
-import Sales from 'pages/sales';
-import Layout from 'layout/Layout';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import 'styles/styles.scss';
->>>>>>> 28a2ab7277131e88e222501978e9f222f27157ac
 
 //Libreria FontAwesome
 import ReactDOM from "react-dom";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { faCheckSquare, faCoffee } from "@fortawesome/free-solid-svg-icons";
+import Layout from "layout/Layout";
 
 library.add(fab, faCheckSquare, faCoffee);
 
@@ -34,15 +23,14 @@ function App() {
   return (
     <div className="App">
       <Router>
-<<<<<<< HEAD
         <Switch>
           <Route path="/rhodesian">
             <RhodesianInfoPage />
           </Route>
           <Route path="/AddSales">
-            <Navbar>
+            <Layout>
               <AddSales />
-            </Navbar>
+            </Layout>
           </Route>
           <Route path="/Navbar">
             <Navbar />
@@ -63,28 +51,6 @@ function App() {
             <Login />
           </Route>
         </Switch>
-=======
-          <Switch>
-            <Route path='/rhodesian'>
-              <RhodesianInfoPage/>
-            </Route>
-            <Route path='/bordercollie'>
-              <Bordercollie/>
-            </Route>
-            <Route path='/Index'>
-              <Index/>
-            </Route>
-            <Route path='/Register'>
-              <Register/>
-            </Route>
-            <Route path='/Sales'>
-              <Sales/>
-            </Route>          
-            <Route path='/'>
-              <Login/>
-            </Route>       
-          </Switch>
->>>>>>> 28a2ab7277131e88e222501978e9f222f27157ac
       </Router>
     </div>
   );
