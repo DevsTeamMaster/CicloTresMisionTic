882819f819885334c9834eb0d166ae1daa0b837d
GPFrederick <gpf95@protonmail.com>
2021-09-29 23:12:20 -0500

    Agregar página de agregar producto y cambio de las rutas en el App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,82 @@
 //import Layout from 'layouts/Layout';
-import Index from "pages";
-import Bordercollie from "pages/bordercollie";
-import RhodesianInfoPage from "pages/rhodesian";
-import Login from "pages/login";
-import Register from "pages/register";
-import Sales from "pages/sales";
-import Navbar from "components/Navbar";
-import AddSales from "pages/addSales";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import "styles/styles.scss";
+import Index from 'pages';
+import Bordercollie from 'pages/bordercollie';
+import RhodesianInfoPage from 'pages/rhodesian';
+import Login from 'pages/login';
+import Register from 'pages/register';
+import Sales from 'pages/sales';
+import Navbar from 'components/Navbar';
+import AddSales from 'pages/addSales';
+import AddProduct from 'pages/AddProduct';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import 'styles/styles.scss';
 
 //Libreria FontAwesome
-import ReactDOM from "react-dom";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fab } from "@fortawesome/free-brands-svg-icons";
-import { faCheckSquare, faCoffee } from "@fortawesome/free-solid-svg-icons";
-import Layout from "layout/Layout";
+import ReactDOM from 'react-dom';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
+import Layout from 'layout/Layout';
 
 library.add(fab, faCheckSquare, faCoffee);
 
 function App() {
   return (
-    <div className="App">
-      <Router>
+    <div className='App'>
+      {/*<Router>
         <Switch>
-          <Route path="/rhodesian">
+          <Route path='/rhodesian'>
             <RhodesianInfoPage />
           </Route>
-          <Route path="/AddSales">
+          <Route path='/AddProduct'>
+            <AddProduct />
+          </Route>
+          <Route path='/AddSales'>
             <Layout>
               <AddSales />
             </Layout>
           </Route>
-          <Route path="/Navbar">
+          <Route path='/Navbar'>
             <Navbar />
           </Route>
-          <Route path="/bordercollie">
+          <Route path='/bordercollie'>
             <Bordercollie />
           </Route>
-          <Route path="/Index">
+          <Route path='/Index'>
             <Index />
           </Route>
-          <Route path="/Register">
+          <Route path='/Register'>
             <Register />
           </Route>
-          <Route path="/Sales">
+          <Route path='/Sales'>
             <Sales />
           </Route>
-          <Route path="/">
+          <Route path='/'>
             <Login />
+          </Route>
+        </Switch>
+      </Router>*/}
+
+      <Router>
+        <Switch>
+          <Route path={['/AddSales', '/AddProduct']}>
+            <Layout>
+              <Switch>
+                <Route path='/AddSales'>
+                  <AddSales />
+                </Route>
+                <Route path='/AddProduct'>
+                  <AddProduct />
+                </Route>
+              </Switch>
+            </Layout>
+          </Route>
+          <Route path={['/']}>
+            <Switch>
+              <Route path='/'>
+                <Login />
+              </Route>
+            </Switch>
           </Route>
         </Switch>
       </Router>
