69758413fc18f8dea95bf0eea93cb451f475aa10
Jorge Cruz <joshiecruz@gmail.com>
2021-09-30 23:42:13 -0500

    Agregando Pagina Maestro de Productos

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 //import Layout from 'layouts/Layout';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import 'styles/styles.scss';
+
+
 import Index from 'pages';
 import Bordercollie from 'pages/bordercollie';
 import RhodesianInfoPage from 'pages/rhodesian';
 import Login from 'pages/login';
 import Register from 'pages/register';
 import Sales from 'pages/sales';
+import IntMastProd  from 'pages/intMastProd';
 import Navbar from 'components/Navbar';
 import AddSales from 'pages/addSales';
 import AddProduct from 'pages/addProduct';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import 'styles/styles.scss';
 
 //Libreria FontAwesome
 import ReactDOM from 'react-dom';
@@ -17,6 +20,7 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
 import Layout from 'layout/Layout';
+
 
 library.add(fab, faCheckSquare, faCoffee);
 
@@ -57,12 +61,9 @@ function App() {
         </Switch>
       </Router>*/}
 
-      <Router>
+<Router>
         <Switch>
-           <Route path='/sales'>
-                <Sales/>
-            </Route>
-          <Route path={['/AddSales', '/AddProduct']}>
+          <Route path={['/AddSales', '/AddProduct','/IntMastProd']}>
             <Layout>
               <Switch>
                 <Route path='/AddSales'>
@@ -70,6 +71,9 @@ function App() {
                 </Route>
                 <Route path='/AddProduct'>
                   <AddProduct />
+                </Route>
+                <Route path='/IntMastProd'>
+                  <IntMastProd />
                 </Route>
               </Switch>
             </Layout>
@@ -84,9 +88,10 @@ function App() {
               </Route>
             </Switch>
           </Route>
-         
         </Switch>
       </Router>
+
+
     </div>
   );
 }
