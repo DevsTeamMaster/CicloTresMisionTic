ffa6d4cb2bd1a9f4fd544bfadb26ad62c37b00be
Alcides <alcides44c@gmail.com>
2021-10-15 21:12:36 -0500

    Autenticacion por Token Completa Front

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -12,7 +12,7 @@ const PrivateRoute = ({ children }) => {
   useEffect(() => {
     const fetchAuth0Token = async () => {
       const accessToken = await getAccessTokenSilently({
-        audience: 'api-autenticacion-devsteammaster',
+        audience: 'https://emp-devsteammaster.us.auth0.com/userinfo',
       });
       localStorage.setItem('token', accessToken);
       
@@ -26,12 +26,53 @@ const PrivateRoute = ({ children }) => {
   if (isLoading) return <div>Loading ...</div>;
 
   if (!isAuthenticated){
-    return //loginwithRedirect();   
+    return (<div>
+      <div className="body">
+        <header class="header">
+          <div class="header__container">
+           
+
+            <Link class="header__logo">DevsTeamMaster</Link>
+
+           <div class="header__toggle">
+              <i class="bx bx-menu" id="header-toggle"></i>
+            </div>
+          </div>
+        </header>
+
+        <div class="nav" id="navbar">
+          <nav class="nav__container">
+            <div>
+              <a href="#" class="nav__link nav__logo">
+                <i class="fab fa-github-alt  nav__icon"></i>
+                <span class="nav__logo-name">MisionTic2021</span>
+              </a>
+              </div>
+
+            <a href="#" class="nav__link nav__logout">
+              <i class="bx bx-log-out nav__icon"></i>
+              <span class="nav__name"><Link to='/'>Back to Login</Link></span>
+            </a>
+          </nav>
+        </div>
+
+        <div className='product-container-ususs'>
+      <div className='rounded-3xl ml-5 mt-5 product-div-usus'>
+        <div className='container-usus'>
+          <div className='container-eyes'>      
+           <h1>No estás autorizado para acceder a este sitio</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+       {/* <div className="contenedor-principal"></div> 
+        */} 
+      </div>
+    </div>);
+    
     
       
-  }
-
-  return <>{children}</>;
+  }else{ return <>{children}</>;} 
 
 };
 
